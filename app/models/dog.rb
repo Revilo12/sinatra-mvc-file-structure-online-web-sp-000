7cb29e8ef08f475ae0a6f0724db6eade2412d175
Oliver Dahl <oliverpdahl@gmail.com>
2020-02-21 09:07:01 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,2 +1,13 @@
 class Dog
-  attr_accessor :name, :age, :breek
+  attr_accessor :name, :age, :breed
+
+  @@all = []
+
+  def initialize(name, age, breed)
+    @name = name
+    @age = age
+    @breed = breed
+  end
+
+  def self.all
+    
